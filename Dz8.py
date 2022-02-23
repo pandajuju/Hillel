1098de8800fec64cb23d09458fefdb015524af66
juliiaustimova <u.juliana.serg@gmail.com>
2022-02-23 17:16:11 +0200

    Month names added

--- Dz8.py
+++ Dz8.py
@@ -3,12 +3,36 @@ month = int(input('Введите номер месяца:'))
 def season(num):
     if month == 1 or month == 2 or month == 12:
         print('Зима')
+        if month == 1:
+            print('Январь')
+        elif month == 2:
+            print('Февраль')
+        else:
+            print('Декабрь')
     elif month == 3 or month == 4 or month == 5:
         print('Весна')
+        if month == 3:
+            print('Март')
+        elif month == 4:
+            print('Апрель')
+        else:
+            print('Май')
     elif month == 6 or month == 7 or month == 8:
         print('Лето')
+        if month == 6:
+            print('Июнь')
+        elif month == 7:
+            print('Июль')
+        else:
+            print('Август')
     elif month == 9 or month == 10 or month == 11:
         print('Осень')
+        if month == 9:
+            print('Сентябрь')
+        elif month == 10:
+            print('Октябрь')
+        else:
+            print('Ноябрь')
     else:
         print('ОШИБКА. Введите число от 1 до 12')
 
