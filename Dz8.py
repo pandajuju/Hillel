1abc0e4476b2b079b85b0291264db0e0e98ce3b2
juliiaustimova <u.juliana.serg@gmail.com>
2022-02-23 16:52:16 +0200

    Add Dz8 with two solutions

--- Dz8.py
+++ Dz8.py
@@ -13,3 +13,17 @@ def season(num):
         print('ОШИБКА. Введите число от 1 до 12')
 
 season(month)
+
+# def season(month):
+#     if month == 1 or month == 2 or month == 12:
+#         return "Зима"
+#     elif month == 3 or month == 4 or month == 5:
+#         return "Весна"
+#     elif month == 6 or month == 7 or month == 8:
+#         return "Лето"
+#     elif month == 9 or month == 10 or month == 11:
+#         return "Осень"
+#     else:
+#         return 'ОШИБКА. Введите число от 1 до 12'
+#
+# print(season(month))
