2d3c58edc9d9562d4443c06211016cf682e0fee7
juliiaustimova <u.juliana.serg@gmail.com>
2022-02-23 10:30:47 +0200

    correction

--- Dz7.py
+++ Dz7.py
@@ -2,7 +2,7 @@ words = input().split()
 dictionary = {}
 for word in words:
     if word not in dictionary:
-        dictionary[word] = 0
+        dictionary[word] = 1
     else:
         dictionary[word] += 1
     print(dictionary[word])
